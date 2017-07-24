9c728a09f23550372c69cbba0cc422dae4ff8d37
123456 <843088961@qq.com>
2017-07-24 10:07:36 +0800

    请求合并代码

--- GPUImageDemo/GPUImageDemo/AppDelegate.m
+++ GPUImageDemo/GPUImageDemo/AppDelegate.m
@@ -17,6 +17,7 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    NSLog(@"我要合并代码");
     return YES;
 }
 
